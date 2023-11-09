067c8cae2c20a9155581722bed975541abc24f76
desantamaria <santamaria.daniel316@gmail.com>
2023-11-08 19:56:02 -0800

    Add Login Form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import "./Login.css";
 function Login() {
-  return <div className="Login">Welcome to NetBooks!</div>;
+  return (
+    <div className="Login">
+      <div className="login-container">
+        <h1>Login</h1>
+
+        <form className="login-form">
+          <input type="text" name="username" placeholder="Username"></input>
+          <input type="text" name="password" placeholder="Password"></input>
+
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
